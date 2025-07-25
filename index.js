7a6dd69b85e295ec3e60de917abcf6b50777c888
sonal <misssonalianil08@gmail.com>
2025-07-25 16:16:57 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl =>
+        new bootstrap.Popover(popoverTriggerEl)
+    );
+});
+
+function handleSearch(event){
+    event.preventDefault();
+    const input = document.getElementById("searchInput").value.toLowerCase().trim();
+
+    if(input.includes("nursing")){
+        window.location.href = "Nursing.html";
+    }else if(input.includes("occasion")){
+        window.location.href = "occasion.html";
+    }else if(input.includes("active")){
+        window.location.href = "active.html";
+    }else{
+        alert("Sorry, no results found for: " + input);
+    }
+}
